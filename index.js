460c75d51447330b903de9f20def2b40933585ab
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-07-23 20:48:35 -0500

    code for adding and deleting department

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2')
+const inputCheck = require('./utils/inputCheck');
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -5,6 +7,92 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Ihatewindows89!',
+    database: 'employee_db'
+    },
+    console.log('Connected to the employee_db database.')
+);
+// Get all candidates
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: rows
+    });
+    });
+});
+// Get a single department
+app.get('/api/department/:id', (req, res) => {
+    const sql = `SELECT * FROM department WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: row
+    });
+    });
+});
+// Delete a department
+app.delete('/api/department/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+        res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+        res.json({
+        message: 'Department not found'
+        });
+    } else {
+        res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+        });
+    }
+    });
+});
+// Create a department
+app.post('/api/department', ({ body }, res) => {
+    const errors = inputCheck(body, 'id', 'name');
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+});
+const sql = `INSERT INTO department (id, name)
+VALUES (?,?,?)`;
+const params = [body.id, body.name];
+
+db.query(sql, params, (err, result) => {
+if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+}
+res.json({
+    message: 'success',
+    data: body
+});
+});
+
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -12,5 +100,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`);
 });
